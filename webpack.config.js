aa09fc796bd663afaa3eca5f3fed4a8620700139
hu.tang <hu.tang@qingteng.cn>
2020-05-21 11:39:08 +0800

    fix: 修复webpack中css的loader

--- webpack.config.js
+++ webpack.config.js
@@ -32,7 +32,7 @@ module.exports = {
       },
       {
         test: /\.s?css$/,
-        use: [MiniCssExtractPlugin, 'css-loader', 'sass-loader']
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
       }
     ]
   },
